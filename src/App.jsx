53c3f0a80736f3c34a9b2c2d08cd0edaca07718f
tuan <tuanphan204@gmail.com>
2022-01-16 08:55:44 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import { Route,  BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Route,  BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import TodoDetail from './pages/TodoDetail';
 import TodoList from './pages/TodoList';
@@ -10,20 +10,12 @@ function App() {
     <div className="App">
       <Router history={history}>
         <div>
-          <Switch>
-            
             <Route exact path="/todo/:id">
               <TodoDetail />
             </Route>
             <Route exact path="/">
               <TodoList />
             </Route>
-            <Route >
-              <div>
-                oops...not found page!
-              </div>
-            </Route>
-          </Switch>
         </div>
       </Router>
 
